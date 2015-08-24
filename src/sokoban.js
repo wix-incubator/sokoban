b9628a6b80af1fa02410203ad9e12568a65a3290
Shai Yallin <shai.yallin@gmail.com>
2015-08-24 15:55:59 +0300

    Sokoban now hangs functions on the prototype

--- src/sokoban.js
+++ src/sokoban.js
@@ -1,45 +1,44 @@
-import DockerContainer from '../src/docker-container';
+import DockerContainer from './docker-container';
 import Promise from 'promise';
 import _ from 'lodash';
 import retry from 'qretry';
 import winston from 'winston';
 
-function Sokoban() {
-    var containers = {};
+var Sokoban = function() {
+    this.containers = {};
+}
 
-    this.provision = function(imageTag, containerName) {
-        containers[containerName] = new DockerContainer(imageTag, containerName);
-        containers[containerName].pullIfNeeded();
-    };
+Sokoban.prototype.provision = function(imageTag, containerName) {
+    this.containers[containerName] = new DockerContainer(imageTag, containerName);
+    this.containers[containerName].pullIfNeeded();
+};
 
-    this.run = function({containerName, bindings, env, barrier, maxRetries}) {
+Sokoban.prototype.run = function({containerName, bindings, env, barrier, maxRetries}) {
 
-        var container = containers[containerName];
-        var host = container.host();
+    var container = this.containers[containerName];
+    var host = container.host();
 
-        return container.run(bindings || {}, env || [])
-            .then(() => retry(() => barrier(host), {maxRetry: maxRetries || 5}))
-            .then(
-                () => {
-                    var containerInfo = {host};
-                    winston.debug(containerName, "ready, returning container info", containerInfo);
-                    return containerInfo;
-                    },
-                (e) => {
-                    winston.error(containerName, "not ready after", maxRetries, ", error is", e);
-                    throw e;
-                }
-            );
-    };
+    return container.run(bindings || {}, env || [])
+        .then(() => retry(() => barrier(host), {maxRetry: maxRetries || 5}))
+        .then(
+            () => {
+                var containerInfo = {host};
+                winston.debug(containerName, "ready, returning container info", containerInfo);
+                return containerInfo;
+                },
+            (e) => {
+                winston.error(containerName, "not ready after", maxRetries, ", error is", e);
+                throw e;
+            }
+        );
+};
 
-    this.dumpAllLogs = function() {
-        return _.reduce(containers, (promise, container) => promise.then(container.printLogs()), Promise.resolve());
-    };
+Sokoban.prototype.dumpAllLogs = function() {
+    return _.reduce(this.containers, (promise, container) => promise.then(container.printLogs()), Promise.resolve());
+};
 
-    this.killAll = function() {
-        return Promise.all(_.map(containers, c => c.kill()));
-    };
-
+Sokoban.prototype.killAll = function() {
+    return Promise.all(_.map(this.containers, c => c.kill()));
 };
 
 export default Sokoban;
